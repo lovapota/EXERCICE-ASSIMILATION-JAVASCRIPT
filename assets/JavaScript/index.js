888e58a9b4dac3bd329cae695dfbaae88c7c9418
lovapota <rfenosoa1@gmail.com>
2022-04-07 16:48:58 -0700

    Appeler des element HTML en JavaScript

---
+++ assets/JavaScript/index.js
@@ -0,0 +1,11 @@
+//DOM selection
+const liste = document.getElementById('list')
+const inpute = document.getElementById('input')
+const adde = document.getElementById('add')
+const cleare= document.getElementById('clear')
+const urle = document.getElementById('url')
+const loade = document.getElementById('load')
+//On recupere le tableau des taches deja existantes
+const taskse = ['Salle de sport', 'Tourner de tutos']
+
+loade.style.background = 'green'
