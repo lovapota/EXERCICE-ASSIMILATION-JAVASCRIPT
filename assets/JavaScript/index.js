65f7a5f0018dd69cf70073e20765ccc53da9f79b
lovapota <rfenosoa1@gmail.com>
2022-04-08 16:23:37 -0700

    Add event sur tout les element HTML

--- assets/JavaScript/index.js
+++ assets/JavaScript/index.js
@@ -6,21 +6,52 @@ const cleare = document.getElementById('clear')
 const urle = document.getElementById('url')
 const loade = document.getElementById('load')
 //On recupere le tableau des taches deja existantes
-const taskse = ['Salle de sport', 'Tourner de tutos']
+const tasks = ['Salle de sport', 'Tourner de tutos']
 
-//Ajuoute chaque tache a la liste a puces
-for (let i = 0; i < taskse.length; i++) {
+
+
+//Fonction pour ajouter les elements
+function taskToDOM(task) {
     //Si on a une chaine de caracter non-vide
-    if(typeof taskse[i] === 'string' && taskse[i]){
+    if(typeof task === 'string' && task){
         const li = document.createElement ('li') //Creer un variable pour les liste
         const p = document.createElement('p')// Creer des variable pour le texte
         const remove = document.createElement('button') // Creer des variables pour le boutton remove
         
-        p.textContent = taskse[i]//Ajouter des texte dans la liste
+        p.textContent = task//Ajouter des texte dans la liste
         remove.textContent = 'REMOVE'//Ajouter un texte sur le boutton
         li.appendChild(p)//mettre texte en enfant de liste
         li.appendChild(remove)//mettre boutton en enfant de liste
 
+        remove.addEventListener('click', () => {
+            liste.removeChild(li)
+        })
+
         liste.insertBefore(li, liste.lastChild)//Inserer les nouveau element dans le document HTML sur une balise donne
     }
 }
+
+//Ajuoute chaque tache a la liste a puces
+tasks.forEach(lova => taskToDOM(lova))
+
+//On gerel'ajout de tache avec le boutton add et Enter
+function newTask() {
+    inpute.focus()
+}
+
+adde.addEventListener('click', newTask)
+inpute.addEventListener('keydown', e => {
+    if (e.key === 'Enter'){
+        newTask()
+    }
+})
+
+//On suppprime la liste du DOM et du navigateur
+cleare.addEventListener('click', () => {
+    liste.innerHTML = ''
+})
+
+//On gere l'importation de tache
+load.addEventListener('click', () => {
+
+})
